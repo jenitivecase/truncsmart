f7003e22d47a966c0401172c2b842dadf114e25b
jenitivecase <jbrussow@gmail.com>
2016-10-05 10:13:01 -0500

    Possibly the final version?
    Updated further. Changed order of tol values. Updated linetrunc
    function.

--- truncsmart_brussow.R
+++ truncsmart_brussow.R
@@ -4,7 +4,8 @@
 ##' specified separator. Use vectorized function, shortenv, to apply to non-scalar
 ##' @param textstring target string to shorten
 ##' @param linewidth approximate length to shorten taking into account tolerance
-##' @param tol number of characters forward/back to check; if single value then only backwards checking
+##' @param tol number of characters back/forward to check; if single value then only backwards checking
+##' JB NOTE - this configuration of tol seemed more logical to me. tol = 1 char for backwards, or two char for back/forwards
 ##' @param capwidth integer specifying the width of capital letters
 ##' @param separator accepts 1- or 2-element vector to separate string; default = c(" ", "_")
 ##' @return shortened character string
@@ -14,26 +15,27 @@
 ##' (truncsmart(test, 5, tol = c(1,2)))
 ##'
 ##' edited 2016-10-04
-##' JB concern - this still just chops it if no logical space is found within tolerance :/
 
-
+#instead of making a bunch of possible strings, my script focuses on finding the appropriate "index" value where 
 truncsmart <- function(textstring, linewidth, tol = c(5, 5), capwidth = 1.2, separator = c(" ", "_")) {
   lets <- unlist(strsplit(textstring, split = "")) #makes character vector into literally a vector of characters
   widthval <- ifelse(sapply(lets, function(x) x %in% LETTERS) == TRUE, capwidth, 1) #assigns larger width values for capital characters
   linelength <- cumsum(widthval) #calculates the cumulative length after each character
-  index <- Position(function(x){x < linewidth}, linelength, right = TRUE)
   if(linelength[length(linelength)] <= linewidth) return(textstring) #if the string is already short enough, return it
-  breaks <- grepl(separator[1], lets) | grepl(separator[2], lets)
-  if(!is.na(tol[2]) && sum(breaks[index:(index+tol[1])]) > 0){
-    index <- Position(function(x){x == TRUE}, breaks[index:(index+tol[1])], right = TRUE)+index-2
-  } else if(sum(breaks[(index-tol[2]):index]) > 0){
-    index <- index-tol[2]+Position(function(x){x == TRUE}, breaks[(index-tol[2]):index], right = TRUE)-2
-  }
-  out <- paste0(lets[1:index], collapse = "")
+  index <- Position(function(x){x < linewidth}, linelength, right = TRUE) #sets the index position to the first adj value > linewidth
+  breaks <- grepl(separator[1], lets) | grepl(separator[2], lets) #creates a vector showing where break characters are
+  if(!is.na(tol[2]) && sum(breaks[(index+1):(index+tol[2])]) > 0){ #if there are 2 tol values and a break in the forward check...
+    #have to add 1 to index so we aren't evaluating it. have to subtract 1 from result to remove trailing break
+    index <- Position(function(x){x == TRUE}, breaks[(index+1):(index+tol[2])], right = TRUE)+index-1 #set index to last break found in range
+  } else if(sum(breaks[(index-tol[1]):(index-1)]) > 0){ #if there is no second tol value or no breaks in forward tol, look backwards
+    #have to subtract 1 from index so we aren't evaluating it. have to subtract 1 from result to remove trailing break
+    index <- index-tol[1]+Position(function(x){x == TRUE}, breaks[(index-tol[1]):(index-1)], right = TRUE)-1
+  }#if neither condition is satisfied, index remains in its initial position.
+  out <- paste0(lets[1:index], collapse = "") #put the string back together
   return(out)
 }
 
-#test
+#testing the new version
 system.time(for(i in 1:10000){truncsmart("This is a string with l o t s o f b r e a k s in it", 30)})
 system.time(for(i in 1:10000){truncsmart("This_is_a_string_with_l_o_t_s_o_f_b_r_e_a_k_s_in_it", 30)})
 system.time(for(i in 1:10000){truncsmart("This is a string with nobreakswhereyoumightwantthemtobe", 30)})
@@ -45,35 +47,20 @@ truncsmart <- Vectorize(truncsmart, USE.NAMES=FALSE)
 
 
 #line breaking test
-linetrunc <- function(textstring, linewidth, tol = c(5, 5), capwidth = 1.2, separator = c(" ", "_")) {
-  lets <- unlist(strsplit(textstring, split = "")) #makes character vector into literally a vector of characters
-  widthval <- ifelse(sapply(lets, function(x) x %in% LETTERS) == TRUE, capwidth, 1) #assigns larger width values for capital characters
-  linelength <- cumsum(widthval) #calculates the cumulative length after each character
-  if(linelength[length(linelength)] <= linewidth) return(textstring) #if the string is already short enough, return it
-  withinlength <- linelength  <= linewidth #vector of T/F values showing the desired cut location
-  withintol <- linelength <= (linewidth + tol[length(tol)]) #extends the acceptable length according to the tolerance value specified
-  letswl <- lets[withinlength] #cuts the string at the length point given by the user
-  letswt <- lets[withintol] #cuts the string at the length point + tolerance
-  if(sum(grepl(as.character(separator[1]), letswl[(length(letswl)-tol[1]):(length(letswl))])) > 0 |
-     sum(grepl(as.character(separator[2]), letswl[(length(letswl)-tol[1]):(length(letswl))])) > 0){
-    #if a logical cut exists at the lower end of the tolerance, take that
-    index <- tail(which(grepl(as.character(separator[1]), letswl[(length(letswl)-tol[1]):(length(letswl))])), n=1) #must use tail in case two breaks exist
-    if(length(index) == 0){index <- tail(which(grepl(as.character(separator[2]), letswl[(length(letswl)-tol[1]):(length(letswl))])), n=1)}
-    letso <- paste0(paste0(lets[1:((length(letswl)-tol[1])+index-2)], collapse = ""), "/n", 
-                    paste0(lets[((length(letswl)-tol[1])+index):length(lets)], collapse = ""))
-  } 
-  if(!is.na(tol[2]) && sum(grepl(as.character(separator[1]), letswt[(length(letswt)-tol[2]):(length(letswt))])) > 0 |
-     sum(grepl(as.character(separator[2]), letswt[(length(letswt)-tol[2]):(length(letswt))])) > 0){
-    #if a logical cut exists at the higher end of the tolerance, that is better!!
-    index <- tail(which(grepl(as.character(separator[1]), letswt[(length(letswt)-tol[2]):(length(letswt))])), n=1)
-    if(length(index) == 0){index <- tail(which(grepl(as.character(separator[2]), letswt[(length(letswt)-tol[2]):(length(letswt))])), n=1)}
-    letso <- paste0(paste0(lets[1:((length(letswt)-tol[2])+index-2)], collapse = ""), "/n", 
-                   paste0(lets[((length(letswt)-tol[2])+index):length(lets)], collapse = ""))
+linetrunc <- function(textstring, linewidth, tol = c(5, 5), capwidth = 1.2, separator = c(" ", "_"), newline = "/n") {
+  lets <- unlist(strsplit(textstring, split = "")) 
+  widthval <- ifelse(sapply(lets, function(x) x %in% LETTERS) == TRUE, capwidth, 1) 
+  linelength <- cumsum(widthval) 
+  if(linelength[length(linelength)] <= linewidth) return(textstring)
+  index <- Position(function(x){x < linewidth}, linelength, right = TRUE) 
+  breaks <- grepl(separator[1], lets) | grepl(separator[2], lets) 
+  if(!is.na(tol[2]) && sum(breaks[(index+1):(index+tol[2])]) > 0){ 
+    index <- Position(function(x){x == TRUE}, breaks[(index+1):(index+tol[2])], right = TRUE)+index-1 
+  } else if(sum(breaks[(index-tol[1]):(index-1)]) > 0){ 
+    index <- index-tol[1]+Position(function(x){x == TRUE}, breaks[(index-tol[1]):(index-1)], right = TRUE)-1
   }
-  if(!exists("letso")){
-    letso <- paste0(paste0(lets[1:length(letswt)], collapse = ""), "/n", paste0(lets[length(letswt):length(lets)], collapse = ""))
-  }
-  return(letso)
+  out <- paste0(paste0(lets[1:index], collapse = ""), newline, paste0(lets[(index+2):length(lets)], collapse = "")) 
+  return(out)
 }
 
 #test
@@ -84,7 +71,7 @@ linetrunc("This is a string with nobreakswhereyoumightwantthemtobe", 30)
 
 ######################################################################################################################################
 #clocking the original version
-truncsmart <- function(textstring, linewidth, tol = c(5, 5), capwidth = 1.2, separator = c(" ", "_")) {
+truncsmart_old <- function(textstring, linewidth, tol = c(5, 5), capwidth = 1.2, separator = c(" ", "_")) {
   if(length(tol) > 2) stop("Please specify 1 or 2 values for tol.")
   lets <- unlist(strsplit(textstring, split = ""))
   caps <- sapply(lets, function(x) x %in% LETTERS)
@@ -132,6 +119,7 @@ truncsmart <- function(textstring, linewidth, tol = c(5, 5), capwidth = 1.2, sep
   out
 }
 
-system.time(for(i in 1:10000){truncsmart("This is a string with l o t s o f b r e a k s in it", 30)})
-system.time(for(i in 1:10000){truncsmart("This_is_a_string_with_l_o_t_s_o_f_b_r_e_a_k_s_in_it", 30)})
-system.time(for(i in 1:10000){truncsmart("This is a string with nobreakswhereyoumightwantthemtobe", 30)})
+system.time(for(i in 1:10000){truncsmart_old("This is a string with l o t s o f b r e a k s in it", 30)})
+system.time(for(i in 1:10000){truncsmart_old("This_is_a_string_with_l_o_t_s_o_f_b_r_e_a_k_s_in_it", 30)})
+system.time(for(i in 1:10000){truncsmart_old("This is a string with nobreakswhereyoumightwantthemtobe", 30)})
+system.time(for(i in 1:10000){truncsmart_old("This is a string with breaks where you might think", 30)})
